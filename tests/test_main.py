72809eac267e6fa11a877243025aafbfea88e60f
Miroslav Kosanović <kosanovic.miroslav98@gmail.com>
2023-12-26 21:04:14 +0100

    Refactor project creation and update
    This commit refactors the project creation and update functionality in the `test_main.py` file. The changes include simplifying the JSON payload for project creation and updating the project information.

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,14 +5,16 @@ client = TestClient(app)
 
 
 def test_create_project():
-    response = client.post(
-        "/projects",
-        json={"name": "Test Project", "description": "This is a test project"},
-    )
+    response = client.post("/projects", json={"name": "Test Project"})
     assert response.status_code == 200
     assert response.json() == {
         "project_id": "1",
-        "project": {"name": "Test Project", "description": "This is a test project"},
+        "project": {
+            "name": "Test Project",
+            "details": None,
+            "documents": [],
+            "logo": None,
+        },
     }
 
 
@@ -20,7 +22,7 @@ def test_get_all_projects():
     response = client.get("/projects")
     assert response.status_code == 200
     assert response.json() == {
-        "1": {"name": "Test Project", "description": "This is a test project"}
+        "1": {"name": "Test Project", "details": None, "documents": [], "logo": None}
     }
 
 
@@ -28,27 +30,18 @@ def test_get_project_info():
     response = client.get("/project/1/info")
     assert response.status_code == 200
     assert response.json() == {
+        "project_id": "1",
         "name": "Test Project",
-        "description": "This is a test project",
+        "details": None,
+        "documents": [],
+        "logo": None,
     }
 
 
 def test_update_project_info():
-    response = client.put(
-        "/project/1/info",
-        json={
-            "name": "Updated Project",
-            "description": "This is an updated test project",
-        },
-    )
+    response = client.put("/project/1/info", json={"name": "Updated Project"})
     assert response.status_code == 200
-    assert response.json() == {
-        "project_id": "1",
-        "project": {
-            "name": "Updated Project",
-            "description": "This is an updated test project",
-        },
-    }
+    assert response.json() == {"project_id": "1", "name": "Updated Project"}
 
 
 def test_delete_project():
