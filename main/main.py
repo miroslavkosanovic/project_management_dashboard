6b121a854c20f68e4a0cbb4abe3383628907e76b
Miroslav Kosanović <kosanovic.miroslav98@gmail.com>
2023-12-28 21:08:44 +0100

    Add import for sqlalchemy.orm.text
    This commit adds an import statement for `sqlalchemy.orm.text` to the `main.py` file. This import is necessary for using the `text` function in the code.

--- main/main.py
+++ main/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String, Text  # noqa: F401
 from sqlalchemy.orm import sessionmaker, declarative_base  # noqa: F401
 from sqlalchemy.exc import OperationalError
+from sqlalchemy.orm import text
 from dotenv import load_dotenv  # noqa: F401
 from pydantic import BaseModel, HttpUrl
 from typing import Optional, List
@@ -46,7 +47,7 @@ def test_db_connection():
     try:
         # Try to establish a connection and execute a query
         session = Session()
-        session.execute(Text("SELECT 1"))
+        session.execute(text("SELECT 1"))
         print("Connection to the database was successful.")
     except OperationalError:
         print("Failed to connect to the database.")
